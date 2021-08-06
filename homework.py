00a58a7180270f33ffcdcb7257a7261961fad0d5
Egor <egorcoders@gmail.com>
2021-08-07 00:17:33 +0800

    It's done, sir!

--- homework.py
+++ homework.py
@@ -1,7 +1,6 @@
 import datetime as dt
 
-now = dt.datetime.now()
-date_format = '%d.%m.%Y'
+DATE_FORMAT = '%d.%m.%Y'
 
 
 class Record:
@@ -9,18 +8,21 @@ class Record:
         self.amount = amount
         self.comment = comment
         if date is None:
-            # Указываем сегодняшнюю дату по-умолчанию
-            self.date = now.date()
+            self.date = dt.date.today()
         else:
-            # Приводим полученную дату к виду date_format
-            self.date = dt.datetime.strptime(date, date_format).date()
+            self.date = dt.datetime.strptime(date, DATE_FORMAT).date()
 
 
-class Calculator():
+class Calculator:
     def __init__(self, limit):
         self.limit = limit
-        self.week_later = dt.date.today() - dt.timedelta(days=7)
         self.records = []
+
+    def get_today_date(self):
+        return dt.date.today()
+
+    def get_week_date(self):
+        return self.get_today_date() - dt.timedelta(days=7)
 
     def add_record(self, record):
         self.records.append(record)
@@ -28,14 +30,14 @@ class Calculator():
     def get_today_stats(self):
         t_amount = 0
         for record in self.records:
-            if record.date == now.date():
+            if record.date == self.get_today_date():
                 t_amount += record.amount
         return t_amount
 
     def get_week_stats(self):
         total_week_amount = 0
         for record in self.records:
-            if now.date() >= record.date > self.week_later:
+            if self.get_today_date() >= record.date > self.get_week_date():
                 total_week_amount += record.amount
         return total_week_amount
 
@@ -54,38 +56,22 @@ class CashCalculator(Calculator):
     def get_today_cash_remained(self, currency):
         self.currency = currency
         t_amount = self.get_today_limit()
-        rub_currency = 'руб'
-        usd_currency = 'USD'
-        eur_currency = 'Euro'
-        wrong_currency_message = 'Указана неверная валюта. Повторите ввод.'
-        positive_message = 'На сегодня осталось'
-        neutral_message = 'Денег нет, держись'
-        negative_message = 'Денег нет, держись: твой долг -'
+        currencies = {'rub': ('руб', 1),
+                      'usd': ('USD', CashCalculator.USD_RATE),
+                      'eur': ('Euro', CashCalculator.EURO_RATE)}
 
+        c_type, c_rate = currencies[currency]
+
+        if self.currency not in currencies:
+            return 'Указана неверная валюта. Повторите ввод.'
+        if t_amount == 0:
+            return 'Денег нет, держись'
         if t_amount > 0:
-            if self.currency == 'rub':
-                return (f'{positive_message} {t_amount} {rub_currency}')
-            elif self.currency == 'usd':
-                t_amount = round((t_amount / CashCalculator.USD_RATE), 2)
-                return (f'{positive_message} {t_amount} {usd_currency}')
-            elif self.currency == 'eur':
-                t_amount = round((t_amount / CashCalculator.EURO_RATE), 2)
-                return (f'{positive_message} {t_amount} {eur_currency}')
-            else:
-                return (f'{wrong_currency_message}')
-        elif t_amount == 0:
-            return (f'{neutral_message}')
+            return (f'На сегодня осталось '
+                    f'{abs(round((t_amount / c_rate), 2))} {c_type}')
         else:
-            if self.currency == 'rub':
-                return (f'{negative_message} {abs(t_amount)} {rub_currency}')
-            elif self.currency == 'usd':
-                t_amount = round((t_amount / CashCalculator.USD_RATE), 2)
-                return (f'{negative_message} {abs(t_amount)} {usd_currency}')
-            elif self.currency == 'eur':
-                t_amount = round((t_amount / CashCalculator.EURO_RATE), 2)
-                return (f'{negative_message} {abs(t_amount)} {eur_currency}')
-            else:
-                return (f'{wrong_currency_message}')
+            return (f'Денег нет, держись: твой долг - '
+                    f'{abs(round((t_amount / c_rate), 2))} {c_type}')
 
 
 class CaloriesCalculator(Calculator):
@@ -94,17 +80,11 @@ class CaloriesCalculator(Calculator):
         self.records = []
 
     def get_calories_remained(self):
-        positive_message = ('Сегодня можно съесть что-нибудь ещё, но с '
-                            'общей калорийностью не более')
-        neutral_message = 'Хватит есть!'
+        positive_message = ('Сегодня можно съесть что-нибудь ещё, '
+                            'но с общей калорийностью не более')
 
-        t_amount = 0
-        for record in self.records:
-            if record.date == now.date():
-                t_amount += record.amount
-        t_amount = self.limit - t_amount
+        t_amount = self.get_today_limit()
 
         if t_amount > 0:
             return (f'{positive_message} {t_amount} кКал')
-        else:
-            return (f'{neutral_message}')
+        return ('Хватит есть!')
