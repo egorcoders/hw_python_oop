137f26a596dd3846fad11bb6f255b3bdd53564a9
Egor <egorcoders@gmail.com>
2021-08-07 21:39:35 +0800

    Calculator (updated) v.3

--- homework.py
+++ homework.py
@@ -25,21 +25,20 @@ class Calculator:
         return self.get_today_date() - dt.timedelta(days=7)
 
     def add_record(self, record):
-        self.records.append(record)
+        return self.records.append(record)
 
     def get_today_stats(self):
-        t_amount = []
-        for record in self.records:
-            if record.date == self.get_today_date():
-                t_amount.append(record.amount)
+        today = self.get_today_date()
+        t_amount = [record.amount for record in self.records
+                    if record.date == today]
         return sum(t_amount)
 
     def get_week_stats(self):
-        total_week_amount = []
-        for record in self.records:
-            if self.get_today_date() >= record.date > self.get_week_date():
-                total_week_amount.append(record.amount)
-        return sum(total_week_amount)
+        today = self.get_today_date()
+        week_ago = self.get_week_date()
+        week_amount = [record.amount for record in self.records
+                       if today >= record.date > week_ago]
+        return sum(week_amount)
 
     def get_today_limit(self):
         return self.limit - self.get_today_stats()
@@ -59,19 +58,17 @@ class CashCalculator(Calculator):
         currencies = {'rub': ('руб', 1),
                       'usd': ('USD', CashCalculator.USD_RATE),
                       'eur': ('Euro', CashCalculator.EURO_RATE)}
-
-        c_type, c_rate = currencies[currency]
-
         if self.currency not in currencies:
             return 'Указана неверная валюта. Повторите ввод.'
+        c_type, c_rate = currencies[currency]
+
         if t_amount == 0:
             return 'Денег нет, держись'
         if t_amount > 0:
             return (f'На сегодня осталось '
                     f'{abs(round((t_amount / c_rate), 2))} {c_type}')
-        else:
-            return (f'Денег нет, держись: твой долг - '
-                    f'{abs(round((t_amount / c_rate), 2))} {c_type}')
+        return (f'Денег нет, держись: твой долг - '
+                f'{abs(round((t_amount / c_rate), 2))} {c_type}')
 
 
 class CaloriesCalculator(Calculator):
